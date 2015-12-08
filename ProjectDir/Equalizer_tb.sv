bfad05308356ed0d37a67cf1ab5710bf66b708ea
Kody Ryan Greenbaum <kgreenbaum@tux-47.cae.wisc.edu>
2015-12-08 17:17:26 -0600

    Broke TB into tasks

--- ProjectDir/Equalizer_tb.sv
+++ ProjectDir/Equalizer_tb.sv
@@ -1,4 +1,4 @@
-//`include "tb_tasks.sv";
+`include "tb_tasks.sv";
 /****************************************************************
  Module to implement a 5 Channel Equalizer testbench.
  Author : Thundercatz			HDL : System Verilog		 	
@@ -62,7 +62,8 @@ ADC128S iA2D (	.clk(clk), .rst_n(rst_n), .SS_n(A2D_SS_n), .SCLK(A2D_SCLK),
 				
 initial begin
 
-findFreq(EXPECTED_FREQ);
+runTests(EXPECTED_FREQ, EXPECTED_AMP);
+
 $stop;
 
 end
@@ -73,15 +74,16 @@ end
 always
 	#1 clk = ~clk;
 
-///////////////////// Find Freq task ////////////////////////////
+///////////////////// Run Tests task ////////////////////////////
 //	This task can find the frequency of the signals at     //
-// aout_lft and aout_rht. The value given to findFreq as a     //
-// param is the expected value of the freq. This is expected   //
-// to be used by running gen_audio to generate an audio input  //
-// signal, setting analog.dat to only allow one channel. Then, //
-// see if the value calculated is equal to the given param.    //
+// aout_lft and aout_rht. The value given to runTests as       //
+// params are the expected values of the frequency and         //
+// amplitude. This is expected to be used by running gen_audio //
+// to generate an audio input signal, setting analog.dat to    //
+// only allow one channel. Then see if the value calculated is //
+// equal to the given param.                                   //
 /////////////////////////////////////////////////////////////////
-task findFreq (	input [15:0] expected_freq);
+task runTests (	input [15:0] expected_freq, input [15:0] expected_amp);
 
 ////////// Internal Variable Declarations ///////////////////////
 integer fptr;		// File handle for writing output
@@ -97,8 +99,6 @@ logic signed [15:0] lft_max, rht_max,
 			lft_min, rht_min;
 logic signed [15:0] lft_prev, rht_prev;
 logic [15:0] lft_freq, rht_freq;
-logic [15:0] freq_diff;
-logic [15:0] amp_diff;
 
 /////////////////// Open File for output ////////////////////////
 fptr = $fopen("audio_out.csv","w");
@@ -130,7 +130,7 @@ for (x = 0; x < 2045; x = x + 1) begin
 	@(posedge LRCLK);
 end
 
-/////////////////// Read the freq from data ////////////////////////
+/////////////////// Read the freq and amp from data /////////////
 for (x = 0; x < 2048; x = x + 1) begin
 	@(posedge LRCLK);
 	$fwrite( fptr,"%f,%f\n", aout_rht, aout_lft );
@@ -169,20 +169,11 @@ end
 // LRCLK period = 48.828 KHz
 rht_freq = 1/((rht_crossing2 - rht_crossing) * 0.00002048005 * 2);
 $display("Rht Freq = %d Hz, EXPECTED FREQ = %d Hz", rht_freq, expected_freq);
-$display("Rht Amp  = %d Hz, EXPECTED AMP = %d Hz", rht_max, EXPECTED_AMP);
+$display("Rht Amp  = %d Hz, EXPECTED AMP = %d Hz", rht_max, expected_amp);
 
-freq_diff = (expected_freq - rht_freq);
-if (freq_diff < 0) 
-	freq_diff = -freq_diff;
-
-amp_diff = (EXPECTED_AMP - rht_max);
-if (amp_diff < 0)
-	amp_diff = -amp_diff;
-
-if (freq_diff < 0.05*expected_freq && )
-	$display("************PASS*************");
-else
-	$display("*****FAIL*******FAIL*******FAIL******");
+//////////////// Test against expected values ///////////////////
+testFreq(expected_freq, rht_freq);
+testAmp(expected_amp, rht_max);
 
 /////////////////// Close output file ///////////////////////////
 $fclose(fptr);
@@ -192,3 +183,4 @@ $stop;
 endtask
 
 endmodule
+
--- ProjectDir/tb_tasks.sv
+++ ProjectDir/tb_tasks.sv
@@ -1,119 +1,54 @@
-/****************************************************************
- Module to implement tasks used in testbench.
- Author : Thundercatz			HDL : System Verilog		 
- Date : 11/30/2015 							
-****************************************************************/ 
-
-///////////////////// Find Freq task ////////////////////////////
-//	This task can find the frequency of the signals at     //
-// aout_lft and aout_rht. The value given to findFreq as a     //
-// param is the expected value of the freq. This is expected   //
-// to be used by running gen_audio to generate an audio input  //
-// signal, setting analog.dat to only allow one channel. Then, //
-// see if the value calculated is equal to the given param.    //
+///////////////////// Test Freq task ////////////////////////////
+//	This takes the actual and expected values of frequency //
+// and checks if they are within 5 percent of eachother, while //
+// printing relevant information about the test. The values    //
+// given to testFreq as params are the expected and actual     //
+// values of the frequency.                                    //
 /////////////////////////////////////////////////////////////////
-task findFreq (	input expected_freq, input reg clk, RST_n, LRCLK,
-		input [15:0] aout_lft, aout_rht);
+task testFreq(input [15:0] expected_freq, actual_freq);
 
 ////////// Internal Variable Declarations ///////////////////////
-integer fptr;		// File handle for writing output
-logic [11:0] x;		// Counter for loops
-
-logic [15:0]	lft_crossing,
-		rht_crossing,
-		lft_crossing2,
-		rht_crossing2;	// Store the indecies of zero crossing
-logic [1:0] 	lft_cross_cnt, 
-		rht_cross_cnt;	// 
-logic signed [15:0] lft_max, rht_max,
-			lft_min, rht_min;
-logic signed [15:0] lft_prev, rht_prev;
-logic [15:0] lft_freq, rht_freq;
 logic [15:0] freq_diff;
 
-/////////////////// Open File for output ////////////////////////
-fptr = $fopen("audio_out.csv","w");
-
-/////////////////// Variable Declaration ////////////////////////
-clk = 0;
-RST_n = 0;
-@(posedge clk);
-@(negedge clk);
-RST_n = 1;
-lft_crossing = 0;
-rht_crossing = 0;
-lft_crossing2 = 0;
-rht_crossing2 = 0;
-lft_cross_cnt = 0;
-rht_cross_cnt = 0;
-lft_max = -3000;
-rht_max = -3000;
-lft_min = 3000;
-rht_min = 3000;
-lft_prev = 0;
-rht_prev = 0;
-
-lft_freq = 0;
-rht_freq = 0;
-
-//////////////// Wait for Queue to fill /////////////////////////
-for (x = 0; x < 2045; x = x + 1) begin
-	@(posedge LRCLK);
-end
-
-/////////////////// Read the freq from data ////////////////////////
-for (x = 0; x < 2048; x = x + 1) begin
-	@(posedge LRCLK);
-	$fwrite( fptr,"%f,%f\n", aout_rht, aout_lft );
-
-	if (aout_rht > rht_max) begin
-		rht_max = aout_rht;
-	end
-
-	if (lft_prev[15] ^ aout_lft[15]) begin
-
-		if (lft_cross_cnt == 0) begin
-			lft_cross_cnt = lft_cross_cnt + 1;
-			lft_crossing = x;
-		end else if (lft_cross_cnt == 1) begin
-			lft_cross_cnt = lft_cross_cnt + 1;
-			lft_crossing2 = x;
-		end
-	end
-
-	if (rht_prev[15] ^ aout_rht[15]) begin
-
-		if (rht_cross_cnt == 0) begin
-			rht_cross_cnt = rht_cross_cnt + 1;
-			rht_crossing = x;
-		end else if (rht_cross_cnt == 1) begin
-			rht_cross_cnt = rht_cross_cnt + 1;
-			rht_crossing2 = x;
-		end
-	end
-
-	lft_prev = aout_lft;
-	rht_prev = aout_rht;
-	
-end
-
-// 1024 samples per LRCLK period, 2ns per clk cycle, 
-// taken from consequtive cross so calculating 1/2 period (Hence 2 at end)
-rht_freq = 1/((rht_crossing2 - rht_crossing) * 1024 * 0.000000002 * 2);
-$display("Rht Freq = %dHz, EXPECTED FREQ = %d", rht_freq, expected_freq);
-
-freq_diff = (expected_freq - rht_freq);
+freq_diff = (expected_freq - actual_freq);
 if (freq_diff < 0) 
 	freq_diff = -freq_diff;
 
-if (freq_diff < 0.1*expected_freq)
-	$display("************PASS*************");
-else
-	$display("*****FAIL*******FAIL*******FAIL******");
+if (freq_diff < 0.05*expected_freq) begin
+	$display("************PASSSING FREQUENCY*************");
+        $display("Percent Error Frequency = %f percent", (100*freq_diff)/expected_freq);
+end
+else begin
+	$display("*****FAIL*******FREQUENCYFAIL*******FAIL******");
+	$display("Percent Error Frequency = %f percent", (100*freq_diff)/expected_freq);
+end
+endtask
 
-/////////////////// Close output file ///////////////////////////
-$fclose(fptr);
 
-$stop;
+///////////////////// Test Amp task /////////////////////////////
+//	This takes the actual and expected values of amplitude //
+// and checks if they are within 5 percent of eachother, while //
+// printing relevant information about the test. The values    //
+// given to testAmp as params are the expected and actual      //
+// values of the amplitude.                                    //
+/////////////////////////////////////////////////////////////////
+task testAmp(input [15:0] expected_amp, actual_amp);
 
+////////// Internal Variable Declarations ///////////////////////
+logic [15:0] amp_diff;
+
+amp_diff = (expected_amp - actual_amp);
+if (amp_diff < 0)
+	amp_diff = -amp_diff;
+
+if (amp_diff < 0.05*expected_amp) begin
+	$display("************PASSSING AMPLITUDE*************");
+	$display("Percent Error Amplitude = %f percent ", (100*amp_diff)/expected_amp);
+
+end
+else begin
+	$display("*****FAIL*******AMPLITUDEFAIL*******FAIL******");
+	$display("Percent Error Amplitude = %f percent ", (100*amp_diff)/expected_amp);
+
+end
 endtask
